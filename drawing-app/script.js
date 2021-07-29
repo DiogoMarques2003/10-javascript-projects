8222b0ccf11248635bf33a404021bc06ea78b953
Diogo Marques <diogomarques2003@protonmail.com>
2021-07-29 16:46:07 +0100

    Drawing APP done

---
+++ drawing-app/script.js
@@ -0,0 +1,78 @@
+const increaseBTN = document.getElementById('increase');
+const decreaseBTN = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEL = document.getElementById('color');
+const clearBTN = document.getElementById('clear');
+const canvasEL = document.getElementById('canvas');
+const ctx = canvasEL.getContext('2d');
+
+let size = 20;
+let mouseIsPressed = false;
+let color = 'black';
+let x = undefined;
+let y = undefined;
+
+canvasEL.addEventListener('mousedown', (e) => {
+    mouseIsPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
+});
+
+canvasEL.addEventListener('mouseup', (e) => {
+    mouseIsPressed = false;
+
+    x = undefined;
+    y = undefined;
+});
+
+canvasEL.addEventListener('mousemove', (e) => {
+    if (!mouseIsPressed) return;
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+});
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2); 
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke();
+}
+
+increaseBTN.addEventListener('click', () => {
+    size += 5;
+    if (size > 50) size = 50;
+    updateSizeEL();
+});
+
+decreaseBTN.addEventListener('click', () => {
+    size -= 5;
+    if (size < 5) size = 5;
+    updateSizeEL();
+});
+
+colorEL.addEventListener('change', (e) => {
+    color = e.target.value;
+});
+
+clearBTN.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvasEL.width, canvasEL.height);
+});
+
+function updateSizeEL() {
+    sizeEL.innerText = size;
+}
